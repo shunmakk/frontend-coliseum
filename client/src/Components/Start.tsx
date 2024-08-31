24ea4b6ae05528230a98d4ee2c56f85ce5c51eee
uni <147740687+shunmakk@users.noreply.github.com>
2024-08-31 17:32:46 +0900

    contentAPIの作成

--- client/src/Components/Start.tsx
+++ client/src/Components/Start.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { Difficulty } from "../types";
-import { auth } from "../firebase";
+// import { auth } from "../firebase";
 
 const Start: React.FC = () => {
   //   const user = auth.currentUser;
--- client/src/utils/AuthContext.tsx
+++ client/src/utils/AuthContext.tsx
@@ -1,7 +1,38 @@
-import React from "react";
+import { User, onAuthStateChanged } from "firebase/auth";
+import React, { createContext, useContext, useEffect, useState } from "react";
+import { auth } from "../firebase";
 
-const AuthContext = () => {
-  return <div>AuthContext</div>;
+interface AuthContextType {
+  user: User | null;
+  loading: boolean;
+}
+
+//contentAPIでユーザーの状態管理
+const AuthContext = createContext<AuthContextType>({
+  user: null,
+  loading: true,
+});
+
+export const useAuth = () => useContext(AuthContext);
+
+export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  const [user, setUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setUser(user);
+      setLoading(false);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user, loading }}>
+      {!loading && children}
+    </AuthContext.Provider>
+  );
 };
-
-export default AuthContext;
