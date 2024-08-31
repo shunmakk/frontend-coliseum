ff636411fe118dcbdcdb5ec43f63079c46ba6967
uni <147740687+shunmakk@users.noreply.github.com>
2024-08-31 17:53:55 +0900

    protectedrouteを作成してcontextapiが適用されるようにする

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,19 +6,51 @@ import Start from "./Components/Start";
 import Quiz from "./Components/Quiz";
 import Complete from "./Components/Complete";
 import Profile from "./Components/Profile";
+import { AuthProvider } from "./utils/AuthContext";
+import ProtectedRoute from "./utils/ProtectedRoute";
 
 const App: React.FC = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Auth />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/start" element={<Start />} />
-        <Route path="/quiz/:difficulty" element={<Quiz />} />
-        <Route path="/complete" element={<Complete />} />
-        <Route path="/profile" element={<Profile />} />
-      </Routes>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Auth />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route
+            path="/start"
+            element={
+              <ProtectedRoute>
+                <Start />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/quiz/:difficulty"
+            element={
+              <ProtectedRoute>
+                <Quiz />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/complete"
+            element={
+              <ProtectedRoute>
+                <Complete />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/profile"
+            element={
+              <ProtectedRoute>
+                <Profile />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 };
 
--- client/src/utils/ProtectedRoute.tsx
+++ client/src/utils/ProtectedRoute.tsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { useAuth } from "./AuthContext";
+import { Navigate } from "react-router-dom";
 
-const ProtectedRoute = () => {
-  return <div>ProtectedRoute</div>;
+interface ProtectedRouteProps {
+  children: React.ReactNode;
+}
+
+const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children }) => {
+  const { user, loading } = useAuth();
+
+  if (loading) {
+    return <div>Loading...!!!</div>;
+  }
+
+  if (!user) {
+    return <Navigate to="/" replace />;
+  }
+
+  return <>{children}</>;
 };
 
 export default ProtectedRoute;
