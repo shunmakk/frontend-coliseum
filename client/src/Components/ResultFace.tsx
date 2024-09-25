f8f593215cfd0fa5468c92cc36c5b8f6713374e5
uni <147740687+shunmakk@users.noreply.github.com>
2024-09-25 23:33:53 +0900

    add: resulticon

---
+++ client/src/Components/ResultFace.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { FaFaceDizzy } from "react-icons/fa6";
+import { FaFaceFrown } from "react-icons/fa6";
+import { FaFaceFrownOpen } from "react-icons/fa6";
+import { FaFaceGrin } from "react-icons/fa6";
+import { FaFaceGrinBeam } from "react-icons/fa6";
+import { FaFaceGrinSquint } from "react-icons/fa6";
+import { IconType } from "react-icons";
+
+interface ResultProps {
+  score?: number;
+}
+
+const ResultFace: React.FC<ResultProps> = ({ score = 0 }) => {
+  const getResultFace = (): IconType => {
+    if (score === 0) return FaFaceDizzy;
+    if (score === 1) return FaFaceFrown;
+    if (score === 2) return FaFaceFrownOpen;
+    if (score === 3) return FaFaceGrin;
+    if (score === 4) return FaFaceGrinBeam;
+    return FaFaceGrinSquint;
+  };
+
+  const ResultIcon = getResultFace();
+
+  return <ResultIcon size={30} />;
+};
+
+export default ResultFace;
