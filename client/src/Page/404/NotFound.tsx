342752325dcaca63b3794cbfb7fa99122d04ad81
uni <147740687+shunmakk@users.noreply.github.com>
2024-10-04 03:18:29 +0900

    404 ui

--- client/src/Page/404/NotFound.tsx
+++ client/src/Page/404/NotFound.tsx
@@ -1,8 +1,28 @@
+import { Alert, AlertIcon, AlertTitle, AlertDescription } from "@chakra-ui/react";
+
 const NotFound = () => {
   return (
-    <div>
-      <p>404 NotFound</p>
-    </div>
+    <>
+      <Alert
+        status="error"
+        variant="subtle"
+        flexDirection="column"
+        alignItems="center"
+        justifyContent="center"
+        textAlign="center"
+        height="200px"
+        className="bg-red-200 rounded-sm"
+        width={600}
+      >
+        <AlertIcon boxSize="40px" mb={4} color="red" />
+        <AlertTitle mt={4} mb={1} className="text-2xl font-semibold">
+          404 NotFound
+        </AlertTitle>
+        <AlertDescription maxWidth="sm" className="mt-1">
+          お探しのページは見つかりません
+        </AlertDescription>
+      </Alert>
+    </>
   );
 };
 
