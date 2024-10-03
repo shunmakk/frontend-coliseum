416183d42f6f26f4cfff9e9acc25c7f4336f36ec
uni <147740687+shunmakk@users.noreply.github.com>
2024-10-03 22:26:41 +0900

    loading animation

--- client/src/Components/LoadingOrError.tsx
+++ client/src/Components/LoadingOrError.tsx
@@ -2,8 +2,9 @@ import React from "react";
 
 const LoadingOrError: React.FC = () => {
   return (
-    <div>
-      <p>ローディング...</p>
+    <div className="flex justify-center items-center flex-col">
+      <div className="animate-spin h-10 w-10 border-4  border-blue-400 rounded-full border-t-transparent"></div>
+      <p className="mt-4">ローディング中</p>
     </div>
   );
 };
